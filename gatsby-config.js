ea026d4a467040da4e33b4c1458e07fa7e1ca0f9
Christian Vari <vari.christian@gmail.com>
2023-03-24 11:32:09 +0100

    implemented gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,47 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require(`dotenv`).config({ path: `.env` });
+
 module.exports = {
-  siteMetadata: {
-    title: `Maria Iacobelli Designer`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Maria Iacobelli Designer`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [process.env.GA_TRACKING_ID],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+    ],
 };
